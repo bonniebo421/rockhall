3847fd6938e919e078dc0df7f4ceaf9b7078e3bd
Bonnie Bohan <bbohan@mos.org>
2015-09-19 11:24:57 -0400

    TPL Updates

--- sites/all/themes/rock/templates/page--front.tpl.php
+++ sites/all/themes/rock/templates/page--front.tpl.php
@@ -2,7 +2,8 @@
 	<?php print render($page['header']); ?>
 </header>
 
-<?php print render($page['content']); ?>
-<?php print render($content['body']); ?>
+<?php // print render($page['content']); ?>
+<?php // print render($content['body']); ?>
+<?php print views_embed_view('homepage_features','homepage_features', $node->nid); ?>
 
 <?php print render($page['footer']); ?>
---
+++ sites/all/themes/rock/templates/views-view--homepage_features.tpl.php
@@ -0,0 +1,33 @@
+<?php 
+$features = array();
+
+foreach ($variables['view']->result as $result) {
+    $node = node_load($result->nid);
+
+    $features[] = array(
+        'image'   => $node->field_video_thumbnail['und'][0]['uri'],
+        'alt'   => $node->field_video_thumbnail['und'][0]['alt'],
+        'title'   => $node->field_video_clip_title['und'][0]['safe_value'],
+        'description' => $node->field_video_description['und'][0]['safe_value'],
+        'url' => $node->field_link_to_interview['und'][0]['url'],
+        'link' => $node->field_link_to_interview['und'][0]['title'],
+        'order' => $node->field_homepage_order['und'][0]['value']
+    );
+}
+
+?> 
+
+<section id="features">
+    <?php foreach ($features as $feature) { ?>
+        <div id="<?php print $feature['order']; ?>">
+            <?php if ($feature['image']) { ?>
+                <img src="<?php print file_create_url($feature['image']); ?>" alt="<?php print $feature['alt']; ?>" />
+            <?php } else { ?>
+                No image
+            <?php } ?>
+            <h3><?php print $feature['title']; ?></h3>
+            <p class="description"><?php print $feature['description']; ?></p>
+            <a href="<?php print file_create_url($feature['url']); ?>"><?php print $feature['link']; ?></a>
+        </div>
+    <?php } ?>
+</section>

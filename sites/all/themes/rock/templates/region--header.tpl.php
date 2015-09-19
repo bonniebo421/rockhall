c457445511462e57357be7337777b5dd6246d8e7
Bonnie Bohan <bbohan@mos.org>
2015-09-19 11:30:10 -0400

    New Header Logo Link

--- sites/all/themes/rock/templates/region--header.tpl.php
+++ sites/all/themes/rock/templates/region--header.tpl.php
@@ -7,5 +7,5 @@
 </div>
 <div id="project-title">
 	<a href="/rockhall"><img id="logo" src="http://www.bonniebohan.com/rockhall/sites/all/themes/rock/images/logo.jpg" alt="Rock and Roll Hall of Fame Oral Histories" /></a>
-	<h1><a href="/">Oral Histories Project</a></h1>
+	<h1><a href="/rockhall">Oral Histories Project</a></h1>
 </div>

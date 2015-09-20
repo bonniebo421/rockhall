3caa4a8c9baf3f9fe643ed5771e8a197a73b2d56
Bonnie Bohan <bbohan@mos.org>
2015-09-20 14:51:19 -0400

    Footer updates and Home Feature Video

--- sites/all/themes/rock/templates/region--footer.tpl.php
+++ sites/all/themes/rock/templates/region--footer.tpl.php
@@ -1,5 +1,9 @@
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
 <footer>
     <div id="copyright">
         <p>Powered by: <a href="http://rockhall.com" target="blank">The Rock and Roll Hall Fame</a></p>
+    </div>
+    <div id="social-media">
+    	<a href="https://www.facebook.com/rockandrollhalloffame" target="_blank" class="fa fa-facebook-square"></a> <a href="https://twitter.com/rock_hall" target="_blank" class="fa fa-twitter-square"></a><a href="http://instagram.com/rockhall" target="_blank" class="fa fa-instagram-square"></a> <a href="http://pinterest.com/rockhall/" target="_blank" class="fa fa-pinterest-square"></a> <a href="http://www.youtube.com/user/rockhall" target="_blank" class="fa fa-youtube-square"></a> <a href="https://plus.google.com/115412030406922618012/posts" target="_blank" class="fa fa-google-plus-square"></a>
     </div>
 </footer>
--- sites/all/themes/rock/templates/views-view--homepage_features.tpl.php
+++ sites/all/themes/rock/templates/views-view--homepage_features.tpl.php
@@ -8,6 +8,7 @@ foreach ($variables['view']->result as $result) {
         'image'   => $node->field_video_thumbnail['und'][0]['uri'],
         'alt'   => $node->field_video_thumbnail['und'][0]['alt'],
         'title'   => $node->field_video_clip_title['und'][0]['safe_value'],
+        'video'   => $node->field_video_id['und'][0]['safe_value'],
         'description' => $node->field_video_description['und'][0]['safe_value'],
         'url' => $node->field_link_to_interview['und'][0]['url'],
         'link' => $node->field_link_to_interview['und'][0]['title'],
@@ -20,12 +21,14 @@ foreach ($variables['view']->result as $result) {
 <section id="features">
     <?php foreach ($features as $feature) { ?>
         <div id="feat-<?php print $feature['order']; ?>">
-            <?php if ($feature['image']) { ?>
+            <?php if ($feature['video']) { ?>
+                <div id="video">
+                    <iframe src='https://player.vimeo.com/video/<?php print $feature['video']; ?>?autoplay=0&portrait=0&byline=0&title=0' width='800' height='480' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
+            <?php } else { ?>
                 <div>
                     <img src="<?php print file_create_url($feature['image']); ?>" alt="<?php print $feature['alt']; ?>" />
                 </div>
-            <?php } else { ?>
-                No image
             <?php } ?>
             <div>
                 <h2><?php print $feature['title']; ?></h2>
